70ab83f8e5c9535271b459bd3ed3154620c8f240
Xiaohan <xiaohan.zhu@outlook.com>
2019-03-09 17:09:18 +0800

    Load data

--- treeSearch/hzTree/forms/forms.py
+++ treeSearch/hzTree/forms/forms.py
@@ -6,7 +6,7 @@ from hzTree.models import QueryUser
 class QueryUserForm(ModelForm):
     class Meta:
         model = QueryUser
-        fields = ['condition', 'queryContent', ]
+        fields = ['condition', 'queryContent', 'queryContent2',]
         # 指定呈现样式字段、指定 CSS 样式
         widgets = {
             'condition': Select(attrs={'class':"form-control",
@@ -16,7 +16,11 @@ class QueryUserForm(ModelForm):
             'queryContent':TextInput(attrs={'class': 'form-control is-invalid',
                                       'name': 'queryContent',
                                       'placeholder': '请输入需要查询的内容...'
-                                      })
+                                      }),
+            'queryContent2':TextInput(attrs={'class': 'form-control is-invalid',
+                'name': 'queryContent',
+                'placeholder': '请输入需要查询的内容...'
+                })
         }
 
         localized = {
--- treeSearch/hzTree/models.py
+++ treeSearch/hzTree/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Tree(models.Model):
-    zname = models.CharField(max_length=64)
-    kname = models.CharField(max_length=64)
+    # 基本信息
+    zname = models.CharField(max_length=64, default=" ")
+    kname = models.CharField(max_length=64, default=" ")
+    sname = models.CharField(max_length=64, default=" ")
+    ldname = models.CharField(max_length=64, default=" ")
+    biename = models.CharField(max_length=64, default=" ")
+    # 观赏特征
+    morphology = models.CharField(max_length=300, default=" ")
+    bloom_date = models.CharField(max_length=64, default=" ")
+    bloom_color = models.CharField(max_length=64, default=" ")
+    fruit_date = models.CharField(max_length=64, default=" ")
+    fruit_color = models.CharField(max_length=64, default=" ")
+    leaf_color = models.CharField(max_length=64, default=" ")
     
     class Meta:
         managed = True
@@ -13,13 +24,25 @@ class Tree(models.Model):
         return self.zname
 
 CONDITION_CHOICES = (
+    # 基本信息
     ('zname', '中文名'),
+    ('ldname', '拉丁名'),
     ('kname', '科名'),
+    ('sname', '属名'),
+    ('biename', '别名'),
+    # 观赏特征
+    ('morphology', '形态'),
+    ('bloom_date', '花期'),
+    ('bloom_color', '花色'),
+    ('fruit_date', '果期'),
+    ('fruit_color', '果色'),
+    ('leaf_color', '叶色'),
 )
 
 class QueryUser(models.Model):
     condition = models.CharField(max_length=20, choices=CONDITION_CHOICES)
     queryContent = models.CharField(max_length=100)
+    queryContent2 = models.CharField(max_length=100)
 
     def __str__(self):        
         return self.condition
